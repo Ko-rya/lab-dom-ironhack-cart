2fcd69d6fc67b706defda48f4599a38b2af5bc7f
Enzo <120275012+Ko-rya@users.noreply.github.com>
2023-06-02 19:29:06 +0200

    Add bonus iterations

--- src/index.js
+++ src/index.js
@@ -22,16 +22,85 @@ total.textContent = totalValue
 
 // ITERATION 4
 
+const removeButtons = document.querySelectorAll(".btn-remove");
+let removeButtonsArray = [...removeButtons];
+removeButtonsArray.forEach(button => {
+  button.addEventListener("click", removeProduct);
+});
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  const parentRow = target.parentNode.parentNode
+  table = parentRow.parentNode
+  table.removeChild(parentRow)
+  calculateAll()
 }
 
+
 // ITERATION 5
+const createButton = document.querySelector("#create");
+createButton.addEventListener("click", createProduct);
 
 function createProduct() {
-  //... your code goes here
+  const newProductNameInput = document.querySelector('.newProduct');
+  const newPriceInput = document.querySelector('.newPrice');
+
+  const newProductName = newProductNameInput.value;
+  const newPrice = parseFloat(newPriceInput.value).toFixed(2);
+
+  const tableBody = document.querySelector('tbody');
+
+  if (newProductName=== "" || newPrice=== "") {
+    return; // Stop the function execution
+  }
+  const newRow = document.createElement('tr');
+  newRow.className = 'product';
+
+ 
+  const nameCell = document.createElement('td');
+  nameCell.className = 'name';
+  const nameSpan = document.createElement('span');
+  nameSpan.textContent = newProductName;
+  nameCell.appendChild(nameSpan);
+  newRow.appendChild(nameCell);
+
+
+  const priceCell = document.createElement('td');
+  priceCell.className = 'price';
+  priceCell.innerHTML = `$<span>${newPrice}</span>`;
+  newRow.appendChild(priceCell);
+
+  
+  const quantityCell = document.createElement('td');
+  quantityCell.className = 'quantity';
+  const quantityInput = document.createElement('input');
+  quantityInput.type = 'number';
+  quantityInput.value = 0;
+  quantityInput.min = 0;
+  quantityInput.placeholder = 'Quantity';
+  quantityCell.appendChild(quantityInput);
+  newRow.appendChild(quantityCell);
+
+
+  const subtotalCell = document.createElement('td');
+  subtotalCell.className = 'subtotal';
+  subtotalCell.innerHTML = '$<span>0</span>';
+  newRow.appendChild(subtotalCell);
+
+
+  const removeCell = document.createElement('td');
+  removeCell.className = 'action';
+  const removeButton = document.createElement('button');
+  removeButton.className = 'btn btn-remove';
+  removeButton.textContent = 'Remove';
+  removeCell.appendChild(removeButton);
+  newRow.appendChild(removeCell);
+
+
+  tableBody.appendChild(newRow);
+
+  
+  newProductNameInput.value = '';
+  newPriceInput.value = '';
 }
 
 window.addEventListener('load', () => {
